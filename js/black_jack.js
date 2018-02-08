b0d73b6dc6c2a10867bfed33e6f1f84c23f9204e
libres <libres@libres.home>
2018-02-08 19:25:22 +0100

    Troisieme commit. Statistiques des victoires, defaites et matches nuls conservés. Remise à zero des scores et du nombre selectionné effectuée par la fonction stop(). Affichage des comparaisons des nombres et de l'annonce de victoire deplacé dans la console.

--- js/black_jack.js
+++ js/black_jack.js
@@ -4,56 +4,56 @@ function getRandomInt(max) {
 }
 
 function start() {
-  /* selection des balises html dans des variables */
-	let selection = document.querySelector("#selectorField");
-	let conteneur = document.querySelector("#result");
+
+  /* Recupere l'id du champ de selection du nombre */
+  let selection = document.querySelector("#selectorField");
+
+  /* Recupere les valeurs des deux nombres */
+  let randomInt = getRandomInt(2);
+  var inputNumber = selection.value;
+
+  /* Recupere le div et le paragraphe ou est affiché le chiffre généré */
+  let conteneur = document.querySelector("#result");
+  let paragrapheContaineur = document.querySelector("#paragraphe");
+
+  /* Recupere le div et le paragraphe ou est affiché la comparaison et l'annonce de victoire */
   let secondConteneur = document.querySelector("#whoWin");
-  let paragrapheContaineur = document.querySelector("#paragraphe")
   let paragrapheVictoire = document.querySelector("#AnnonceDeVictoire");
+
+  /* Recupere le div et les paragraphes ou sont affichés les scores */
   let containeurCompteur = document.querySelector("#compteur");
-
-
-  /* Récuperation des valeurs des nombres random et choisis */
-  let randomInt = getRandomInt(10);
-  let inputNumber = selection.value;
+  let containeurVictoire = document.querySelector("#compteurVictoire");
+  let containeurDefaite = document.querySelector("#compteurDefaite");
+  let containeurMatchNul = document.querySelector("#compteurMatchNul");
+  let textContaineurVictoire = document.createTextNode("Nombre total de victoires");
 
   /* Insertion des nombres dans leurs paragraphes */
 	let textParagraphe = document.createTextNode(randomInt);
 	paragraphe.appendChild(textParagraphe);
 	conteneur.appendChild(paragraphe);
 
-  let defaite = 0;
-  let victoire = 0;
-  let matchNul = 0;
-
   /* Comparaison des nombres at affichage du vainqueur */
   if(randomInt == inputNumber) {
-    let contenuVictoire = document.createTextNode(" Le nombre choisi est \
-      égal au nombre généré. Vous avez gagné !");
-      paragrapheVictoire.appendChild(contenuVictoire);
-      secondConteneur.appendChild(paragrapheVictoire);
-      console.log(randomInt);
-      console.log(inputNumber);
-      matchNul+= 1;
+    console.log(" Égalité !");
+    scoreEgalite = document.querySelector("#scoreEgalite");
+      scoreEgalite.value ++;
     }
   else if(randomInt > inputNumber) {
-    let contenuVictoire = document.createTextNode(" Le nombre choisi est \
-        inferieur au nombre généré. Vous avez gagné !");
-        paragrapheVictoire.appendChild(contenuVictoire);
-        secondConteneur.appendChild(paragrapheVictoire);
-        victoire +=1;
+    console.log(" Le nombre choisi est inferieur au nombre généré. Vous avez gagné !");
+    scoreVictoire = document.querySelector("#scoreVictoire");
+    scoreVictoire.value++;
       }
   else if(randomInt < inputNumber){
-    let contenuVictoire = document.createTextNode(" Le nombre choisi est \
-      supérieur au nombre généré. Vous avez perdu !");
-      paragrapheVictoire.appendChild(contenuVictoire);
-      secondConteneur.appendChild(paragrapheVictoire);
-      defaite += 1;
+    console.log(" Le nombre choisi est supérieur au nombre généré. Vous avez perdu !");
+    scoreDefaite = document.querySelector("#scoreDefaite");
+    scoreDefaite.value++;
     }
-  let containeurVictoire = document.querySelector("#compteurVictoire");
-  let containeurDefaite = document.querySelector("#compteurDefaite");
-  let containeurMatchNul = document.querySelector("#compteurMatchNul");
-  containeurCompteur.appendChild(containeurVictoire);
-  containeurCompteur.appendChild(containeurDefaite);
-  containeurCompteur.appendChild(containeurMatchNul);
+}
+
+function stop() {
+
+  selectorField.value = 0;
+  scoreEgalite.value = 0;
+  scoreVictoire.value = 0;
+  scoreDefaite.value = 0;
 }
